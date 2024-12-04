979961bb27d5bc8caf0066df843742b2e96dbe4e
GigaTortilla <martinappel01@gmail.com>
2024-12-04 23:31:59 +0100

    created empty constructor and solution methods for day 3

---
+++ src/Day03.java
@@ -0,0 +1,15 @@
+import java.io.FileNotFoundException;
+
+public class Day03 extends AoC {
+    Day03(String filePath) throws FileNotFoundException {
+        super(filePath);
+    }
+
+    public int part1() {
+        return 0;
+    }
+
+    public int part2() {
+        return 0;
+    }
+}
