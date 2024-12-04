bcbe55d47eddec3453e2dc7ecbe22945bba074ec
GigaTortilla <martinappel01@gmail.com>
2024-12-04 22:08:37 +0100

    reading input reports as list of list of ints

--- src/Day02.java
+++ src/Day02.java
@@ -1,7 +1,24 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Day02 extends AoC {
+    List<List<Integer>> reports = new ArrayList<List<Integer>>();
+
     Day02(String filePath) throws FileNotFoundException {
         super(filePath);
+        Scanner sc = new Scanner(new File(filePath));
+        while (sc.hasNextLine()) {
+            List<Integer> l = new ArrayList<Integer>();
+            String line = sc.nextLine();
+            for (String s : line.split(" ")) {
+                l.add(Integer.parseInt(s));
+            }
+            reports.add(l);
+        }
+        sc.close();
+        System.out.println(reports);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Day01 solution = new Day01("inputs/day01.txt");
-        System.out.println("The solution for " + solution.getClass().getName() + " part 1 is " + solution.part1());
-        System.out.println("The solution for " + solution.getClass().getName() + " part 2 is " + solution.part2());
+        Day01 solution01 = new Day01("inputs/day01.txt");
+        System.out.println("The solution01 for " + solution01.getClass().getName() + " part 1 is " + solution01.part1());
+        System.out.println("The solution01 for " + solution01.getClass().getName() + " part 2 is " + solution01.part2());
+
+        Day02 solution02 = new Day02("inputs/day02.txt");
     }
 }
