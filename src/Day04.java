09cb8c256d7f40eb0dd4cfb14482abd6ec413cee
GigaTortilla <martinappel01@gmail.com>
2024-12-05 21:49:25 +0100

    solution for part 1

--- src/Day04.java
+++ src/Day04.java
@@ -1,40 +1,82 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Day04 extends AoC {
-    private final String puzzle;
-    private int cols;
+    private final List<String> lines = new ArrayList<>();
     private final String searchString = "XMAS";
+    private final String reverseSearchString = new StringBuilder(searchString).reverse().toString();
 
     Day04(String filePath) throws FileNotFoundException {
         super(filePath);
-        StringBuilder sb = new StringBuilder();
-        Scanner sc = new Scanner(dayInput);
+        Scanner sc = new Scanner(new File(filePath));
         while (sc.hasNextLine()) {
-            String line = sc.nextLine();
-            cols = line.length();
-            sb.append(line);
+            lines.add(sc.nextLine());
         }
-        puzzle = sb.toString();
     }
 
-    private String diagonalWord(int pos) {
-        int remainingLength = puzzle.length() - pos;
-        if (remainingLength <= (searchString.length() - 1) * cols + searchString.length() - 1) {
-            return "";
-        }
+    // X
+    //  M
+    //   A
+    //    S
+    private String diagonalWord(int line, int pos) throws IndexOutOfBoundsException {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < searchString.length(); i++) {
-            sb.append(puzzle.charAt(pos + (i + i * cols)));
+            sb.append(lines.get(line + i).charAt(pos + i));
+        }
+        return sb.toString();
+    }
+
+    //    X
+    //   M
+    //  A
+    // S
+    private String reverseDiagonalWord(int line, int pos) throws IndexOutOfBoundsException {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < searchString.length(); i++) {
+            sb.append(lines.get(line + i).charAt(pos - i + searchString.length() - 1));
+        }
+        return sb.toString();
+    }
+
+    // X
+    // M
+    // A
+    // S
+    private String verticalWord(int line, int pos) throws IndexOutOfBoundsException {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < searchString.length(); i++) {
+            sb.append(lines.get(line + i).charAt(pos));
         }
         return sb.toString();
     }
 
     public int part1() {
         int count = 0;
-        for (int i = 0; i < puzzle.length(); i++) {
-            System.out.println(diagonalWord(i));
+        // search vertical and horizontal words separately to include the border
+        for (int i = 0; i < lines.size() - (searchString.length() - 1); i++) {
+            for (int j = 0; j < lines.get(i).length() - (searchString.length() - 1); j++) {
+                String dWord = diagonalWord(i, j);
+                String rdWord = reverseDiagonalWord(i, j);
+                if (searchString.equals(dWord) || reverseSearchString.equals(dWord)) count++;
+                if (searchString.equals(rdWord) || reverseSearchString.equals(rdWord)) count++;
+            }
+
+            for (int j = 0; j < lines.get(i).length(); j++) {
+                String vWord = verticalWord(i, j);
+                if (searchString.equals(vWord) || reverseSearchString.equals(vWord)) count++;
+            }
         }
+
+        for (String line : lines) {
+            for (int j = 0; j < line.length() - (searchString.length() - 1); j++) {
+                String hWord = line.substring(j, j + searchString.length());
+                if (searchString.equals(hWord) || reverseSearchString.equals(hWord)) count++;
+            }
+        }
+
         return count;
     }
 
