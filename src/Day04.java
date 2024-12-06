dab1ffa64d728470c94b40d7aa1cbb8f185e055e
GigaTortilla <martinappel01@gmail.com>
2024-12-06 08:39:46 +0100

    completed day 4

--- src/Day04.java
+++ src/Day04.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 
 public class Day04 extends AoC {
     private final List<String> lines = new ArrayList<>();
-    private final String searchString = "XMAS";
-    private final String reverseSearchString = new StringBuilder(searchString).reverse().toString();
+    private final String searchString;
+    private final String reverseSearchString;
 
     Day04(String filePath) throws FileNotFoundException {
         super(filePath);
@@ -15,28 +15,30 @@ public class Day04 extends AoC {
         while (sc.hasNextLine()) {
             lines.add(sc.nextLine());
         }
+        searchString = "XMAS";
+        reverseSearchString = new StringBuilder(searchString).reverse().toString();
     }
 
     // X
     //  M
     //   A
     //    S
-    private String diagonalWord(int line, int pos) throws IndexOutOfBoundsException {
+    private String diagonalWord(int line, int pos, int length) throws IndexOutOfBoundsException {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < searchString.length(); i++) {
+        for (int i = 0; i < length; i++) {
             sb.append(lines.get(line + i).charAt(pos + i));
         }
         return sb.toString();
     }
 
-    //    X
-    //   M
-    //  A
-    // S
-    private String reverseDiagonalWord(int line, int pos) throws IndexOutOfBoundsException {
+    //    X(0)
+    //   M(1)
+    //  A(2)
+    // S(3)
+    private String reverseDiagonalWord(int line, int pos, int length) throws IndexOutOfBoundsException {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < searchString.length(); i++) {
-            sb.append(lines.get(line + i).charAt(pos - i + searchString.length() - 1));
+        for (int i = 0; i < length; i++) {
+            sb.append(lines.get(line + i).charAt(pos - i + length - 1));
         }
         return sb.toString();
     }
@@ -45,9 +47,9 @@ public class Day04 extends AoC {
     // M
     // A
     // S
-    private String verticalWord(int line, int pos) throws IndexOutOfBoundsException {
+    private String verticalWord(int line, int pos, int length) throws IndexOutOfBoundsException {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < searchString.length(); i++) {
+        for (int i = 0; i < length; i++) {
             sb.append(lines.get(line + i).charAt(pos));
         }
         return sb.toString();
@@ -55,24 +57,30 @@ public class Day04 extends AoC {
 
     public int part1() {
         int count = 0;
-        // search vertical and horizontal words separately to include the border
+        // search until border for the diagonal words
         for (int i = 0; i < lines.size() - (searchString.length() - 1); i++) {
             for (int j = 0; j < lines.get(i).length() - (searchString.length() - 1); j++) {
-                String dWord = diagonalWord(i, j);
-                String rdWord = reverseDiagonalWord(i, j);
+                String dWord = diagonalWord(i, j, searchString.length());
+                String rdWord = reverseDiagonalWord(i, j, searchString.length());
+                String vWord = verticalWord(i, j, searchString.length());
+                String hWord = lines.get(i).substring(j, j + searchString.length());
                 if (searchString.equals(dWord) || reverseSearchString.equals(dWord)) count++;
                 if (searchString.equals(rdWord) || reverseSearchString.equals(rdWord)) count++;
+                if (searchString.equals(vWord) || reverseSearchString.equals(vWord)) count++;
+                if (searchString.equals(hWord) || reverseSearchString.equals(hWord)) count++;
             }
 
-            for (int j = 0; j < lines.get(i).length(); j++) {
-                String vWord = verticalWord(i, j);
+            // search right border for vertical words only
+            for (int j = lines.get(i).length() - (searchString.length() - 1); j < lines.get(i).length(); j++) {
+                String vWord = verticalWord(i, j, searchString.length());
                 if (searchString.equals(vWord) || reverseSearchString.equals(vWord)) count++;
             }
         }
 
-        for (String line : lines) {
-            for (int j = 0; j < line.length() - (searchString.length() - 1); j++) {
-                String hWord = line.substring(j, j + searchString.length());
+        // search bottom border for horizontal words
+        for (int i = lines.size() - (searchString.length() - 1); i < lines.size(); i++) {
+            for (int j = 0; j < lines.get(i).length() - (searchString.length() - 1); j++) {
+                String hWord = lines.get(i).substring(j, j + searchString.length());
                 if (searchString.equals(hWord) || reverseSearchString.equals(hWord)) count++;
             }
         }
@@ -80,7 +88,20 @@ public class Day04 extends AoC {
         return count;
     }
 
+    // M S
+    //  A
+    // M S
     public int part2() {
-        return 0;
+        int count = 0;
+        String fwd = "MAS";
+        String bwd = new StringBuilder(fwd).reverse().toString();
+        for (int i = 0; i < lines.size() - (fwd.length() - 1); i++) {
+            for (int j = 0; j < lines.get(i).length() - (fwd.length() - 1); j++) {
+                String dWord = diagonalWord(i, j, fwd.length());
+                String rdWord = reverseDiagonalWord(i, j, fwd.length());
+                if ((dWord.equals(fwd) || dWord.equals(bwd)) && (rdWord.equals(fwd) || rdWord.equals(bwd))) count++;
+            }
+        }
+        return count;
     }
 }
