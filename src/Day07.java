de8b7d2bad016c70adaad6d21eda1b4427518e60
GigaTortilla <martinappel01@gmail.com>
2024-12-09 01:47:17 +0100

    solution for part 1 and 2 using recursion for optimization

--- src/Day07.java
+++ src/Day07.java
@@ -3,108 +3,63 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 public class Day07 extends AoC {
-    List<BinaryOperator<Long>> operators = List.of(
-            Long::sum,
-            ( x, y) -> x * y
-    );
-    List<List<Long>> operands = new ArrayList<>();
-    List<Long> solutions = new ArrayList<>();
+
+    List<Equation> equations = new ArrayList<>();
+    List<Equation> eqWithConcat = new ArrayList<>();
 
     Day07(String filePath) throws FileNotFoundException {
         super(filePath);
         Scanner sc = new Scanner(dayInput);
         while (sc.hasNextLine()) {
-            String[] line = sc.nextLine().split(":");
-            solutions.add(Long.parseLong(line[0]));
-
-            String[] numbers = line[1].trim().split(" ");
-            List<Long> numList = Arrays.stream(numbers).map(Long::parseLong).collect(Collectors.toList());
-            operands.add(numList);
+            String[] line = sc.nextLine().split(": ");
+            equations.add(equationFromLine(line, 2));
+            eqWithConcat.add(equationFromLine(line, 3));
         }
         sc.close();
     }
 
-    private List<Long> calculateResults(
-            List<Long> remainingOperands,
-            List<BinaryOperator<Long>> operators,
-            List<Long> results,
-            List<Boolean> concatUsed) {
-        // base case: no operands to add or multiply
-        if (remainingOperands.isEmpty()) {
-            return results;
-        }
-        List<Long> operands = new ArrayList<>(remainingOperands);
-        if (results.isEmpty()) {
-            results.add(operands.getFirst());
-            concatUsed.add(false);
-            operands.removeFirst();
-            return calculateResults(operands, operators, results, concatUsed);
-        } else {
-            List<Long> newResults = new ArrayList<>();
-            List<Boolean> newConcatUsed = new ArrayList<>();
-            Long rightOperand = operands.getFirst();
-            for (int i = 0; i < operands.size(); i++) {
-                for (int j = 0; j < operators.size(); j++) {
-                    if (j != 3 || !concatUsed.get(i)) {
-                        newResults.add(operators.get(j).apply(operands.get(i), rightOperand));
-                    }
-                    newConcatUsed.add(3 == j);
-                }
+    private Equation equationFromLine(String[] line, int base) {
+        long result = Long.parseLong(line[0]);
+        List<Long> values = Arrays.stream(line[1].split(" ")).map(Long::parseLong).collect(Collectors.toList());
+        List<List<Integer>> operators = new ArrayList<>();
+        for (int i = 0; i < Math.pow(base, values.size() - 1); i++) {
+            List<Integer> operator = new ArrayList<>();
+            int num = i;
+            for (int j = 0; j < values.size() - 1; j++) {
+                operator.add(num % base);
+                num /= base;
             }
-            operands.removeFirst();
-            return calculateResults(operands, operators, newResults, newConcatUsed);
+            operators.add(operator);
         }
-    }
-
-
-    private List<Long> calculateResults(
-            List<Long> remainingOperands,
-            List<BinaryOperator<Long>> operators,
-            List<Long> results) {
-        // base case: no operands to add or multiply
-        if (remainingOperands.isEmpty()) {
-            return results;
-        }
-        List<Long> operands = new ArrayList<>(remainingOperands);
-        if (results.isEmpty()) {
-            results.add(operands.getFirst());
-            operands.removeFirst();
-            return calculateResults(operands, operators, results);
-        } else {
-            List<Long> newResults = new ArrayList<>();
-            Long rightOperand = operands.getFirst();
-            for (Long leftOperand : results) {
-                for (BinaryOperator<Long> operator : operators) {
-                    newResults.add(operator.apply(rightOperand, leftOperand));
-                }
-            }
-            operands.removeFirst();
-            return calculateResults(operands, operators, newResults);
-        }
+        return (new Equation(result, values, operators));
     }
 
     public long partOne() {
-        long sum = 0;
-        for (int i = 0; i < solutions.size(); i++) {
-            if (calculateResults(operands.get(i), operators, new ArrayList<>()).contains(solutions.get(i))) {
-                sum += solutions.get(i);
-            }
-        }
-        return sum;
+        return equations.stream()
+                .filter(equation -> equation.calculateResults().contains(equation.target))
+                .mapToLong(equation -> equation.target).sum();
+    }
+
+    public long partOneRecursive() {
+        return equations.stream()
+                .filter(equation -> equation.checkEquation(false))
+                .mapToLong(equation -> equation.target).sum();
     }
 
     public long partTwo() {
+        return eqWithConcat.stream()
+                .filter(equation -> equation.calculateResults().contains(equation.target))
+                .mapToLong(equation -> equation.target).sum();
+    }
+
+    public long partTwoRecursive() {
         long sum = 0;
-        List<BinaryOperator<Long>> opWithConcat = new ArrayList<>(operators);
-        opWithConcat.add((x, y) -> Long.parseLong(x + Long.toString(y)));
-        for (int i = 0; i < solutions.size(); i++) {
-            if (calculateResults(operands.get(i), opWithConcat, new ArrayList<>(), new ArrayList<>()).contains(solutions.get(i))) {
-                sum += solutions.get(i);
-            }
+        for (Equation equation : equations) {
+            if (equation.checkEquation(true))
+                sum += equation.target;
         }
         return sum;
     }
